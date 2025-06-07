b987b556371dc68dc849a0a3f5783785ae571638
kawatchan <124563070+kawatchan@users.noreply.github.com>
2025-06-08 00:48:11 +0800

    Update BaseTemplate.tsx

--- src/templates/BaseTemplate.tsx
+++ src/templates/BaseTemplate.tsx
@@ -1,37 +1,14 @@
-import { useTranslations } from 'next-intl';
-import { AppConfig } from '@/utils/AppConfig';
-
 export const BaseTemplate = (props: {
   leftNav: React.ReactNode;
   rightNav?: React.ReactNode;
   children: React.ReactNode;
 }) => {
-  const t = useTranslations('BaseTemplate');
-
   return (
     <div className="w-full px-1 text-gray-700 antialiased">
       <div className="mx-auto max-w-screen-md">
-        {/* 头部全部移除，只保留 main 和 footer */}
         <main>{props.children}</main>
-
         <footer className="border-t border-gray-300 py-8 text-center text-sm">
-          {`© Copyright ${new Date().getFullYear()} ${AppConfig.name}. `}
-          {t.rich('made_with', {
-            author: () => (
-              <a
-                href="https://creativedesignsguru.com"
-                className="text-blue-700 hover:border-b-2 hover:border-blue-700"
-              >
-                CreativeDesignsGuru
-              </a>
-            ),
-          })}
-          {/*
-           * PLEASE READ THIS SECTION
-           * I'm an indie maker with limited resources and funds, I'll really appreciate if you could have a link to my website.
-           * The link doesn't need to appear on every pages, one link on one page is enough.
-           * For example, in the `About` page. Thank you for your support, it'll mean a lot to me.
-           */}
+          © Copyright 2025 LUVTRY Design Studio.
         </footer>
       </div>
     </div>
