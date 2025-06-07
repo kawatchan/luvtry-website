9898bcaca493e4992dfc561f932fec5fdbbeed87
kawatchan <124563070+kawatchan@users.noreply.github.com>
2025-06-08 00:28:48 +0800

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -9,24 +9,9 @@ import '@/styles/global.css';
 export const metadata: Metadata = {
   icons: [
     {
-      rel: 'apple-touch-icon',
-      url: '/apple-touch-icon.png',
-    },
-    {
       rel: 'icon',
-      type: 'image/png',
-      sizes: '32x32',
-      url: '/favicon-32x32.png',
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/favicon-16x16.png',
-    },
-    {
-      rel: 'icon',
-      url: '/favicon.ico',
+      type: 'image/svg+xml',
+      url: '/assets/images/logo.svg', // 路径与你 public 文件夹一致
     },
   ],
 };
